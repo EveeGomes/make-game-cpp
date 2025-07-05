8bbc904c2401bdb557877b539c1b4a9145d195fa
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2025-07-05 16:59:11 -0300

    Change to use smallest = randomNumbers[0]

--- sec2-fundamentals-of-cpp/tasks/task04.cpp
+++ sec2-fundamentals-of-cpp/tasks/task04.cpp
@@ -8,12 +8,15 @@
 /*
  * 1st: 
  *    generate a bunch of random numbers to work with.
+ * 2nd:
+ *    check each number and compare with a variable that holds the latest smaller number.
 */
 
 int main()
 {
-   const int MAXNUMBERS = 10;
-
+   const int MAXNUMBERS = 0;
+   //int smallest = std::numeric_limits<int>::max();
+   int smallest = 0;
    std::vector<int> randomNumbers;
 
    // Seed the random number generator
@@ -29,6 +32,27 @@ int main()
 
       std::cout << randomNumbers[i] << " ";
    }
+   std::cout << std::endl;
+   
+   // Taking care of the possibility when the vector of numbers is empty, and not using numeric_limits to set smallest
+   if (!randomNumbers.empty())
+   {
+      smallest = randomNumbers[0];
+      for (int number : randomNumbers)
+      {
+         if (number < smallest)
+         {
+            smallest = number;
+         }
+      }
+
+      std::cout << "The smallest number is: " << smallest;
+   }
+   else
+   {
+      std::cout << "There are no numbers to check";
+   }
+   std::cout << std::endl;
 
    return 0;
 }
