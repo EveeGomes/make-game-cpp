30a87fe06179f433f2517de74741da0d5046e92d
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2025-11-21 08:42:42 -0300

    Create a function to copy all sfml DLLs to where the exe file is

--- LightYears/LightYears/src/main.cpp
+++ LightYears/LightYears/src/main.cpp
@@ -6,10 +6,10 @@ int main()
    sf::Vector2u VectorWindow { 100,100 };
    sf::RenderWindow renderWindow{ sf::VideoMode(VectorWindow), "My Window"};
 
-   while (renderWindow.isOpen())
-   {
-
-   }
+   //while (renderWindow.isOpen())
+   //{
+   //
+   //}
    
    std::cout << "Hello World!" << std::endl;
 }
