435999eab681cb0e4e06ca7993d30d348b592502
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2025-11-21 08:27:16 -0300

    Add a command for post build events. This is for adding .dlls to the same directory of the executable file .exe.

--- LightYears/LightYears/src/main.cpp
+++ LightYears/LightYears/src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+   sf::Vector2u VectorWindow { 100,100 };
+   sf::RenderWindow renderWindow{ sf::VideoMode(VectorWindow), "My Window"};
+
+   while (renderWindow.isOpen())
+   {
+
+   }
+   
    std::cout << "Hello World!" << std::endl;
 }
